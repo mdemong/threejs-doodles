11b25a0a5192caf4a1ffed50da644278e26d0c55
mdemong <mdemong1@gmail.com>
2019-08-12 14:46:26 -0700

    spinny stars

---
+++ 5-stars2/js/stars.js
@@ -0,0 +1,76 @@
+const STAR_SIZE = 0.12;
+
+let camera, scene, renderer;
+let farClip = 100;
+let density = 10;
+let speed = 0.2;
+let spread = 500;
+
+// Treat like a queue; push and shift 
+let stars = []
+
+init();
+animate();
+
+function init() {
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, farClip);
+    camera.position.set(0, 0, 0);
+    for (let i = 0; i < farClip; i += speed) {
+        addMultStars(-i);
+    }
+
+    renderer = new THREE.WebGLRenderer({ antialias: true, canvas: jsCanvas });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    window.addEventListener("resize", onWindowResize, false);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    addMultStars(camera.position.z - farClip);
+    animateStars();
+    camera.translateZ(-speed);
+    renderer.render(scene, camera);
+    removeLayer();
+}
+
+function animateStars() {
+    for (var star of stars) {
+        let theta = (star.position.z - camera.position.z) / 8
+        star.position.y += Math.sin(theta) * .035;
+        star.position.x += Math.cos(theta) * .035;
+    }
+}
+
+function addMultStars(posZ) {
+    for (let i = 0; i < density; i++) {
+        addStar(posZ);
+    }
+}
+
+function addStar(posZ) {
+    let starGeom = new THREE.SphereBufferGeometry(STAR_SIZE);
+    let material = new THREE.MeshBasicMaterial;
+    let star = new THREE.Mesh(starGeom, material);
+
+    posX = Math.random() * spread - (spread / 2);
+    posY = Math.random() * spread - (spread / 2);
+    star.position.set(posX, posY, posZ);
+    scene.add(star);
+    stars.push(star);
+}
+
+function removeLayer() {
+    for (let i = 0; i < density; i++) {
+        scene.remove(scene.children[0]);
+        stars.shift();
+    }
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
